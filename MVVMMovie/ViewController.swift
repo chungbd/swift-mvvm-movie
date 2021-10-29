ae1377f60cdd09b2ded13b8efc6b4543e5099e78
Chung Bui <chungbd90@gmail.com>
2021-10-29 11:06:04 +0700

    Initial Commit

---
+++ MVVMMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMMovie
+//
+//  Created by Bui Chung on 29/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
